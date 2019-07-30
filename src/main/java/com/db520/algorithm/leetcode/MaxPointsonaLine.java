a15365729fac8949b36b84dbe8bc7bade2a41465
zhuyl <zyl@caini.cn>
2019-07-30 10:32:24 +0800

    Add MaxPointsonaLine.java

--- src/main/java/com/db520/algorithm/leetcode/MaxPointsonaLine.java
+++ src/main/java/com/db520/algorithm/leetcode/MaxPointsonaLine.java
@@ -1,5 +1,9 @@
 package com.db520.algorithm.leetcode;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 149. Max Points on a Line
  *
@@ -12,11 +16,88 @@ package com.db520.algorithm.leetcode;
 public class MaxPointsonaLine {
 
     public static void main(String[] args) {
-        int[][] points = {{1, 1}, {2, 2}, {3, 3}};
+        //int[][] points = {{1, 1}, {2, 2}, {3, 3}};
+        //int[][] points = {{0, 0}, {1, 1}, {0, 0}};
+        int[][] points = {{1, 1}, {1, 1}, {1, 1}};
+        //int[][] points = {{1,1},{3,2},{5,3},{4,1},{2,3},{1,4}};
+        //int[][] points = {{0,0},{94911151,94911150},{94911152,94911151}};
         System.out.println(new MaxPointsonaLine().maxPoints(points));
     }
 
     public int maxPoints(int[][] points) {
-        return 0;
+        if(points.length <= 2) {
+            return points.length;
+        }
+        int result = 0;
+        for (int i = 0; i < points.length; i++) {
+            Map<Long, Integer> map = new HashMap<>();
+            int samex = 1;
+            int samexy = 0;
+
+            for (int j = 0; j < points.length; j++) {
+                if(j != i) {
+                    if(points[j][0] == points[i][0] && points[j][1] == points[i][1]) {
+                        samexy++;
+                    }
+                    if(points[j][0] == points[i][0]) {
+                        samex++;
+                        continue;
+                    }
+                    int gcd = getGcd(points[j][0] - points[i][0], points[j][1] - points[i][1]);
+                    long k = ((long)((points[j][1] - points[i][1]) / gcd) << 32) + ((points[j][0] - points[i][0]) / gcd);
+                    if(map.containsKey(k)) {
+                        map.put(k, map.get(k) + 1);
+                    } else {
+                        map.put(k, 2);
+                    }
+                    result = Math.max(result, map.get(k) + samexy);
+                }
+            }
+            result = Math.max(result, samex);
+        }
+
+        return result;
+    }
+
+    int getGcd(int a, int b) {
+        return b == 0 ? a : getGcd(b, a % b);
+    }
+
+    //too slow
+    public int maxPoints2(int[][] points) {
+        if(points.length <= 2) {
+            return points.length;
+        }
+        int result = 0;
+        for (int i = 0; i < points.length; i++) {
+            Map<BigDecimal, Integer> map = new HashMap<>();
+            int samex = 1;
+            int samexy = 0;
+
+            for (int j = 0; j < points.length; j++) {
+                if(j != i) {
+                    if(points[j][0] == points[i][0] && points[j][1] == points[i][1]) {
+                        samexy++;
+                    }
+                    if(points[j][0] == points[i][0]) {
+                        samex++;
+                        continue;
+                    }
+                    //这里是考虑到了Java的Double的精度问题，所以才是用BigDecimal的问题
+                    BigDecimal fenziBigDecimal=new BigDecimal(points[i][1]-points[j][1]);
+                    BigDecimal fenmuBigDecimal=new BigDecimal(points[i][0]-points[j][0]);
+                    BigDecimal k = fenziBigDecimal.divide(fenmuBigDecimal,20, BigDecimal.ROUND_HALF_DOWN);
+                    if(map.containsKey(k)) {
+                        map.put(k, map.get(k) + 1);
+                    } else {
+                        map.put(k, 2);
+                    }
+                    result = Math.max(result, map.get(k) + samexy);
+                }
+            }
+            result = Math.max(result, samex);
+        }
+
+        return result;
     }
 }
